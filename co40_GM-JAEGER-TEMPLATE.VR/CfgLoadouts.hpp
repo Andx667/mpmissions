9317babfc1fcbcc6eb06c3fccfec07b9f34a667e
Andx <andx667@protonmail.com>
2025-04-14 01:36:17 +0200

    bw!

--- co40_GM-JAEGER-TEMPLATE.VR/CfgLoadouts.hpp
+++ co40_GM-JAEGER-TEMPLATE.VR/CfgLoadouts.hpp
@@ -16,7 +16,7 @@ class CfgLoadouts {
         uniform[] = {"gm_ge_uniform_soldier_90_flk"};
         backpack[] = {""};
         vest[] = {"gm_ge_vest_armor_90_rifleman_flk"};
-        headgear[] = {"gm_ge_headgear_m92_flk"};
+        headgear[] = {"gm_ge_headgear_m92_flk", "gm_ge_headgear_m92_glasses_flk"};
 
         //Gasmaske geht auch auf den Goggles Slot -> Camo ist weg
         //goggles[] = {"gm_headgear_foliage_summer_forest_01", "gm_headgear_foliage_summer_forest_02", "gm_headgear_foliage_summer_forest_03", "gm_headgear_foliage_summer_forest_04"};
@@ -80,9 +80,11 @@ class CfgLoadouts {
         vest[] = {"gm_ge_vest_armor_90_machinegunner_flk"};
 
         primary[] = {"gm_mg3_blk","gm_120Rnd_762x51mm_B_T_DM21_mg3_grn","","",""};
+        secondary[] = {"gm_p210_blk","gm_8Rnd_9x19mm_B_DM51_p210_blk","",""};
 
         magazines[] += {
-            "gm_120Rnd_762x51mm_B_T_DM21_mg3_grn", 2
+            "gm_120Rnd_762x51mm_B_T_DM21_mg3_grn", 2,
+            "gm_8Rnd_9x19mm_B_DM51_p210_blk", 2
         };
 
         items[] += {
@@ -111,7 +113,7 @@ class CfgLoadouts {
         roledescription = "JÄGER U. SCHARFSCHTZ";
         isplayerloadout = true;
 
-        primary[] = {"gm_g3sg1_oli", "gm_diavari_da_stanagClaw_oli", "gm_20Rnd_762x51mm_B_T_DM21A2_g3_blk","",""};
+        primary[] = {"gm_g3sg1_oli", "gm_diavari_da_stanagClaw_oli", "gm_20Rnd_762x51mm_B_T_DM21A2_g3_blk","gm_g3_bipod_blk",""};
 
         magazines[] += {
             "gm_20Rnd_762x51mm_B_T_DM21A2_g3_blk", 8,
@@ -144,31 +146,76 @@ class CfgLoadouts {
         isplayerloadout = true;
 
         backpack[] = {"gm_ge_army_backpack_medic_80_oli"};
+
+        items[] += {
+            "ACE_SurgicalKit",
+            "ACE_Suture",30,
+
+            "ACE_PackingBandage",10,
+            "ACE_ElasticBandage",30,
+            "ACE_Tourniquet",5,
+
+            "ACE_SalineIV",5,
+            "ACE_SalineIV_500",3,
+            "ACE_SalineIV_250",3,
+            "ACE_Painkillers",5,
+            "ACE_Morphine",5,
+            "ACE_Epinephrine",5
+        };
     };
 
     class B_medic_F: B_Soldier_F {
         roledescription = "JÄGER UND San-B";
         isplayerloadout = true;
 
-        vest[] = {"gm_ge_vest_90_medic_flk"};
+        vest[] = {"gm_ge_vest_armor_90_medic_flk"};
+        backpack[] = {"gm_ge_army_backpack_90_flk"};
+        items[] += {
+            "ACE_SurgicalKit",
+            "ACE_PersonalAidKit",
+            "ACE_Suture",60,
+
+            "ACE_PackingBandage",30,
+            "ACE_ElasticBandage",60,
+            "ACE_Tourniquet",10,
+
+            "ACE_SalineIV",16,
+            "ACE_SalineIV_500",8,
+            "ACE_SalineIV_250",8,
+            "ACE_Painkillers",8,
+            "ACE_Morphine",8,
+            "ACE_Epinephrine",8
+        };
+    };
+
+    class B_soldier_exp_F: B_Soldier_F {
+        roledescription = "Pionier";
+        isplayerloadout = true;
+
+        vest[] = {"gm_ge_vest_armor_90_demolition_flk"};
         backpack[] = {"gm_ge_army_backpack_90_flk"};
 
+        secondary[] = {"ACE_VMH3","","","",""};
+
         items[] += {
-            "ACE_personalAidKit",
-            "ACE_surgicalKit",
-            "ACE_morphine", 10,
-            "ACE_epinephrine", 10,
-            "ACE_quikclot", 10,
-            "ACE_tourniquet", 4,
-            "ACE_packingBandage", 60,
+            "ACE_wirecutter",
+            "ACE_EntrenchingTool",
+            "ACE_Clacker",
+            "ACE_DefusalKit",
+            "gm_explosive_petn_charge", 3,
+            "SatchelCharge_Remote_Mag", 1,
+            "ATMine_Range_Mag",
         };
+
+        preLoadout = "";
     };
 
     class B_Soldier_TL_F: CommonBlufor {
         roledescription = "Stellv. Gruppenführer";
         isplayerloadout = true;
 
-        vest[] = {"gm_ge_vest_armor_90_leader_flk"};
+        //Kommentar entfernen für etwas weniger Realismus
+        //vest[] = {"gm_ge_vest_armor_90_leader_flk"};
 
         magazines[] += {
             "gm_30Rnd_556x45mm_B_T_DM21_g36_blk", 9,
@@ -177,7 +224,7 @@ class CfgLoadouts {
         items[] += {
             "mts_whistle_FOX40",
             "ace_flags_red",
-            "ACRE_SEM52SL",
+            //"ACRE_SEM52SL",
         };
     };
 
@@ -192,6 +239,10 @@ class CfgLoadouts {
         magazines[] += {
             "gm_1Rnd_265mm_flare_single_grn_gc", 5,
             "gm_1Rnd_265mm_flare_multi_red_gc", 5,
+        };
+
+        items[] += {
+            "ACRE_SEM52SL",
         };
     };
 
@@ -225,6 +276,17 @@ class CfgLoadouts {
     class B_officer_F: B_Soldier_SL_F {
         roledescription = "JG OFFZ u. ZUGFHR OFFZU";
         isplayerloadout = true;
+
+        backpack[] = {};
+
+        primary[] = {"gm_mp2a1_blk", "gm_32Rnd_9x19mm_B_DM51_mp2_blk", "","",""};
+
+        magazines[] = {
+            "gm_32Rnd_9x19mm_B_DM51_mp2_blk", 6,
+            "gm_handgrenade_frag_dm51a1", 2,
+            "gm_smokeshell_grn_gc",
+            "gm_smokeshell_wht_gc",2,
+        };
 
         items[] += {
             "ACRE_SEM52SL",
